7e3a3b7d04f664ae9c9fd4dbe19ef8bf99a2543c
Danish <muhammadd03@GMAIL.COM>
2024-07-16 19:53:57 +0500

    Update UsageTrack.tsx

--- app/dashboard/_components/UsageTrack.tsx
+++ app/dashboard/_components/UsageTrack.tsx
@@ -16,7 +16,7 @@ import { UpdateCreditUsageContext } from '@/app/(context)/UpdateCreditUsageConte
     const {user}=useUser();
     const {totalUsage,setTotalUsage}=useContext(TotalUsageContext)
     const {userSubscription,setUserSubscription}=useContext(UserSubscriptionContext);
-    const [maxWords,setMaxWords]=useState(50000)
+    const [maxWords,setMaxWords]=useState(100000)
     const {updateCreditUsage,setUpdateCreditUsage}=useContext(UpdateCreditUsageContext);
     useEffect(()=>{
         user&&GetData();
